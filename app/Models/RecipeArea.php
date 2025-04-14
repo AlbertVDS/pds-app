41118a969122e4b1e04de1ccf0229c0cde0b93f3
AlbertVDS <stingandroid@gmail.com>
2025-04-14 11:33:24 +0200

    created recipes, areas, measurements, tags migrations and models

---
+++ app/Models/RecipeArea.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RecipeArea extends Model
+{
+    use HasFactory;
+    /**
+     * The table associated with the model.
+     * @var string
+     */
+    protected $table = 'recipe_areas';
+
+    /**
+     * The attributes that are mass assignable.
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+}
