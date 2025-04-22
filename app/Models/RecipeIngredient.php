0e5c94cbfa2f61de89151094eb09ce15a9c6046e
AlbertVDS <stingandroid@gmail.com>
2025-04-22 15:45:41 +0200

    Link ingredients with food

--- app/Models/RecipeIngredient.php
+++ app/Models/RecipeIngredient.php
@@ -3,11 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class RecipeIngredient extends Model
 {
+    /**
+     * Attributes which are cast as set type
+     * @var array
+     */
+    protected $casts = [
+        'food_ids' => 'array',
+    ];
+
     protected $table = 'recipe_ingredients';
     protected $fillable = [
         'name',
     ];
+
+    /**
+     * Foods associated with this recipe ingredient
+     * @return Collection
+     */
+    public function foods(): Collection
+    {
+        return $this->food_ids ? Food::whereIn('id', $this->food_ids)->orderBy('name')->get() : new Collection();
+    }
 }
