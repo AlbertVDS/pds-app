ad90257e2e950df28b5796381854ac313081bbc8
AlbertVDS <stingandroid@gmail.com>
2025-04-22 15:45:41 +0200

    Link ingredients with food

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -90,4 +90,21 @@ class FoodController extends Controller
     {
         //
     }
+
+    /**
+     * Get results based on the search term.
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function autocomplete(Request $request)
+    {
+        $query = Food::select("name", "id");
+
+        $request->filled('q') ? $query->where('name', 'LIKE', '%' . $request->get('q') . '%') : null;
+        $query->orderBy('name');
+        $query->take(20);
+        $data = $query->get();
+
+        return response()->json($data);
+    }
 }
