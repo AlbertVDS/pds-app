60060804cfd1cf6bf9ad2172580221cfa3828fe9
AlbertVDS <stingandroid@gmail.com>
2025-04-25 13:12:35 +0200

    Added isadmin middleware and added it to web route, register, login, logout

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\FoodController;
 use App\Http\Controllers\FoodSubstitutesController;
 use App\Http\Controllers\MailingController;
-use App\Http\Controllers\MailDataController;
 use App\Http\Controllers\RecipeAreaController;
 use App\Http\Controllers\RecipeCategoryController;
 use App\Http\Controllers\RecipeController;
@@ -11,7 +10,7 @@ use App\Http\Controllers\RecipeTagController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\RecipeIngredientController;
 use App\Http\Controllers\UserController;
-use App\Models\RecipeIngredient;
+use App\Http\Middleware\IsAdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,17 +19,36 @@ Route::get('/', function () {
     ]);
 });
 
+Route::middleware([IsAdminMiddleware::class])->group(function () {
+    Route::resource('foods', FoodController::class);
+    Route::post('save-linked-foods', [RecipeIngredientController::class, 'saveLinkedFoods'])->name('save-linked-foods');
+    Route::post('save-substitute', [FoodSubstitutesController::class, 'updateSubstitute'])->name('save-substitute');
+    Route::resource('mailings', MailingController::class);
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+});
+
+// Auth routes
+Route::post('login', [UserController::class, 'login'])->name('login');
+Route::get('logout', [UserController::class, 'logout'])->name('logout');
+Route::post('register', [UserController::class, 'register'])->name('register');
+
+// Autocomplete routes
 Route::get('area-autocomplete', action: [RecipeAreaController::class, 'autocomplete'])->name('area-autocomplete');
 Route::get('category-autocomplete', action: [RecipeCategoryController::class, 'autocomplete'])->name('category-autocomplete');
 Route::get('food-autocomplete', action: [FoodController::class, 'autocomplete'])->name('food-autocomplete');
 Route::get('ingredient-autocomplete', action: [RecipeIngredientController::class, 'autocomplete'])->name('ingredient-autocomplete');
 Route::get('tag-autocomplete', action: [RecipeTagController::class, 'autocomplete'])->name('tag-autocomplete');
-Route::resource('foods', FoodController::class);
-Route::post('save-linked-foods', [RecipeIngredientController::class, 'saveLinkedFoods'])->name('save-linked-foods');
-Route::post('save-substitute', [FoodSubstitutesController::class, 'updateSubstitute'])->name('save-substitute');
-Route::resource('mailings', MailingController::class);
+
+// Food routes
+Route::resource('foods', FoodController::class)->except([
+    'create',
+    'edit',
+    'update',
+    'destroy'
+]);
+
+// Recipe routes
 Route::match(['get', 'post'], 'recipes', [RecipeController::class, 'index']);
 Route::get('recipes/{id}', [RecipeController::class, 'show'])->name('recipes.show');
 Route::get('recipe-ingredients', [RecipeIngredientController::class, 'index']);
-Route::resource('roles', RoleController::class);
-Route::resource('users', UserController::class);
