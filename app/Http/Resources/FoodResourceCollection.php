e1d298717b30f362dd96e8371f5b02800d0a659c
AlbertVDS <stingandroid@gmail.com>
2025-04-25 11:11:59 +0200

    added api with food and recipe endpoints

---
+++ app/Http/Resources/FoodResourceCollection.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FoodResourceCollection extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'type' => $this->getTypeName(),
+            'level' => $this->level,
+            'weight' => $this->weight,
+            'fructose' => $this->fructose,
+            'lactose' => $this->lactose,
+            'mannitol' => $this->mannitol,
+            'sorbitol' => $this->sorbitol,
+            'GOS' => $this->GOS,
+            'fructan' => $this->fructan,
+        ];
+    }
+}
