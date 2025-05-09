6b7d20391dca733f20dfb9cc8409fa1d04d5f29c
AlbertVDS <stingandroid@gmail.com>
2025-05-09 09:33:09 +0200

    Added mailing CRUD, command, and schedule

--- routes/console.php
+++ routes/console.php
@@ -6,4 +6,8 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::call(function () {
     Artisan::call('app:recipe-sync');
-})->weekly();
+})->weekly()->description('Sync recipes with recipe API');
+
+Schedule::call(function () {
+    Artisan::call('mailing:send');
+})->everyFiveMinutes()->description('Send scheduled mail');
