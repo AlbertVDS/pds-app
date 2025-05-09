7b8fb1b5fd37f1015e09fa4b9ed6adfc7fc2db2e
AlbertVDS <stingandroid@gmail.com>
2025-05-09 10:22:00 +0200

    added soft delete to models

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Collection;
 use App\Models\FoodSubstitute;
 
 class Food extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
     /**
      * The table associated with the model.
      * @var string
--- app/Models/MailingGroup.php
+++ app/Models/MailingGroup.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MailingGroup extends Model
 {
+    use SoftDeletes;
+
     /**
      * The table associated with the model.
      * @var string
